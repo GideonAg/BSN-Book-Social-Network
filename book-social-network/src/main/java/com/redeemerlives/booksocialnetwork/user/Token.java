d587ccc0a59cd9fb805b662d8633ee1976e0175d
GideonAg <gideonagbosu72@gmail.com>
2024-08-29 10:48:36 +0000

    Added Role and Token entities

---
+++ book-social-network/src/main/java/com/redeemerlives/booksocialnetwork/user/Token.java
@@ -0,0 +1,27 @@
+package com.redeemerlives.booksocialnetwork.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @Column(nullable = false, name = "userId")
+    private User user;
+}
